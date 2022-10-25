32093998bb2c8a4df3c3f29cbc78b634936880a2
tracer-ics2022 <tracer@njuics.org>
2022-10-25 20:51:48 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:51:48 up 52 min,  1 user,  load average: 0.49, 0.33, 0.30

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,7 +2,6 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
-#include <string.h>
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
@@ -13,74 +12,78 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-char *itos(int integer)
+int number_of_int(int integer)
   {
-    char *str=NULL;
-    int index=0;
-    if(integer==0)
+    int num=0;
+    int a=10;
+    while(true)
       {
-        *str++='0';
-        *str='\0';
-        return str;
-      }
-    for(int i=0;i<10;i++)
-      {
-        if(integer!=0)
+        if(integer<a)
           {
-            str[i]=(char)((integer%10)+'0');
-            integer/=10;
-            index+=1;
+            return num;
           }
         else
           {
-            str[i]='\0';
-            break;
+            a*=10;
+            num+=1;
           }
       }
-    for(int i=0;i<index/2;i++)
-      {
-        str[i]=str[i]+str[index-1-i];
-        str[index-1-i]=str[i]-str[index-1-i];
-        str[i]=str[i]-str[index-1-i];
-      }
-    return str;
   }
+
+int pow(int n)
+  {
+    int a=10;
+    for(int j=0;j<n;j++)
+      {
+        a*=10;
+      }
+    return a;
+  }
+
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
   char *out1=out;
-  int ret_num=0;
   va_start(ap,fmt);
   int integer;
   char *str;
-  int fmt_len=strlen(fmt);
-  for(int i=0;i<fmt_len;i++)
+  while(*fmt!='\0')
     {
-      if(fmt[i]!='%')
+      if(*fmt!='%')
         {
-          out1[ret_num]=fmt[i];
-          ret_num+=1;
+          *out1++=*fmt++;
         }
       else
         {
-          i++;
-          if(fmt[i]=='d')
+          ++fmt;
+          if(*fmt=='d')
             {
               integer=va_arg(ap,int);
-              str=itos(integer);
-              strcat(out1,str);
-              ret_num+=strlen(str);
+              if(integer<0)
+                {
+                  *out1++='-';
+                  integer=-integer;
+                }
+              int index=number_of_int(integer);
+              for(int i=index;i>=0;i--)
+                {
+                  *out1++=(char)((integer/pow(i))+'0');
+                  integer=integer%pow(i);
+                }
             }
-          else if(fmt[i]=='s')
+          else if(*fmt=='s')
             {
               str=va_arg(ap,char *);
-              strcat(out1,str);
-              ret_num+=strlen(str);
+              while(*str!='\0')
+                {
+                  *out1++=*str++;
+                }
             }
         }
     }
+  
   va_end(ap);
-  return ret_num;
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
