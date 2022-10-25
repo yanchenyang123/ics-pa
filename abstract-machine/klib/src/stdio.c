9ba68d09774ace3401890a4c5e3f202d16b2a212
tracer-ics2022 <tracer@njuics.org>
2022-10-25 20:28:50 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:28:50 up 29 min,  1 user,  load average: 0.22, 0.15, 0.15

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,74 +13,74 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-void int_to_str(char *out1,int integer,int num)
+char *itos(int integer)
   {
-    char str[10]={0};
-    int i=0;
+    char *str=NULL;
+    int index=0;
     if(integer==0)
       {
-        *out1++='0';
-        num+=1;
-        *out1='\0';
-        return ;
+        *str++='0';
+        *str='\0';
+        return str;
       }
-    while(integer)
+    for(int i=0;i<10;i++)
       {
-        str[i]=(char)(integer%10+'0');
-        integer/=10;
-        i++;
+        if(integer!=0)
+          {
+            str[i]=(char)((integer%10)+'0');
+            integer/=10;
+            index+=1;
+          }
+        else
+          {
+            str[i]='\0';
+            break;
+          }
       }
-    for(int j=0;j<i;j++)
+    for(int i=0;i<index/2;i++)
       {
-        *out1++=str[i-1-j];
-        num+=1;
+        str[i]=str[i]+str[index-1-i];
+        str[index-1-i]=str[i]-str[index-1-i];
+        str[i]=str[i]-str[index-1-i];
       }
-    *out1='\0';
-    return ;
-
+    return str;
   }
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
   char *out1=out;
-  int num=0;
+  int ret_num=0;
   va_start(ap,fmt);
   int integer;
   char *str;
-  while(*fmt!='\0')
+  int fmt_len=strlen(fmt);
+  for(int i=0;i<fmt_len;i++)
     {
-      if(*fmt!='%')
+      if(fmt[i]!='%')
         {
-          *out1++=*fmt++;
-          num+=1;
+          out1[ret_num]=fmt[i];
+          ret_num+=1;
         }
       else
         {
-          if(*++fmt=='d')
+          i++;
+          if(fmt[i]=='d')
             {
               integer=va_arg(ap,int);
-              if(integer<0)
-                {
-                  *out1++='-';
-                  num+=1;
-                  int_to_str(out1,integer,num);
-                }
-              else
-                {
-                  int_to_str(out1,integer,num);
-                }
+              str=itos(integer);
+              strcat(out1,str);
+              ret_num+=strlen(str);
             }
-          else if(*++fmt=='s')
+          else if(fmt[i]=='s')
             {
               str=va_arg(ap,char *);
-              num+=strlen(str);
               strcat(out1,str);
+              ret_num+=strlen(str);
             }
         }
-      
     }
   va_end(ap);
-  return num;
+  return ret_num;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
