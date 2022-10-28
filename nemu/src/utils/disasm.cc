8a9c234ea60ccafc9210a54174efbc80f7886132
tracer-ics2022 <tracer@njuics.org>
2022-10-28 19:27:38 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:27:38 up 44 min,  1 user,  load average: 0.29, 0.17, 0.17

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -95,7 +95,8 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
 
   std::string s;
   raw_string_ostream os(s);
-  gIP->printInst(&inst, pc, "111", *gSTI, os);
+  if(1==2)
+  gIP->printInst(&inst, pc, "", *gSTI, os);
 
   int skip = s.find_first_not_of('\t');
   const char *p = s.c_str() + skip;
