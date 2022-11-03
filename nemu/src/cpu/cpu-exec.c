89ae893b214df51f1712d2f06e4f81891c6db190
tracer-ics2022 <tracer@njuics.org>
2022-11-03 20:42:07 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:42:07 up  2:07,  1 user,  load average: 0.52, 0.29, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,7 +98,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #endif
 
 #ifdef CONFIG_IRINGBUF
-  iringbuf[num]=s->logbuf;
+  strcpy(iringbuf[num],s->logbuf);
   puts(s->logbuf);
   num=(num+1)%MAX_Iringbuf_Size;
 #endif
