677a885edcf9c9359e09554fd46a1317cf7bb0a8
tracer-ics2022 <tracer@njuics.org>
2022-11-16 21:14:55 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:14:55 up  2:22,  1 user,  load average: 1.21, 0.80, 0.63

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -50,7 +50,7 @@ static void fail(void *buf) { panic("access nonexist register"); }
 bool ioe_init() {
   for (int i = 0; i < LENGTH(lut); i++)
     if (!lut[i]) 
-    {printf("%d\n",i);
+    {
       lut[i] = fail;}
   __am_gpu_init();
   __am_timer_init();
