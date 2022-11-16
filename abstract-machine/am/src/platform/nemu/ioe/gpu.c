eff7ee354d401f101a5c47c6a2838802aa14dd36
tracer-ics2022 <tracer@njuics.org>
2022-11-16 21:48:16 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:48:16 up  2:56,  1 user,  load average: 0.26, 0.39, 0.58

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -18,7 +18,7 @@ void __am_gpu_init() {
   int w=400;
   int h=300;
   uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
-  for(i=0;i<w*h;i++) fb[i]=1;
+  for(i=0;i<w*h;i++) fb[i]=100;
   outl(SYNC_ADDR,1);
 }
 
