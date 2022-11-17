abb08a35f5ac7ecb521ad98720709ba9eae47c9a
tracer-ics2022 <tracer@njuics.org>
2022-11-17 23:21:31 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:21:31 up  1:36,  1 user,  load average: 0.41, 0.32, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,6 +2,7 @@
 #include <nemu.h>
 #include<stdio.h>
 #include <riscv/riscv.h>
+#include<string.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 //static int w_=0;
 //static int h_=0;
@@ -44,6 +45,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+  memset((void *)0xa1000000, 0, 400*300*32);
+
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
