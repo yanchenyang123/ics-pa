17e5b890b3a2783affe09fdadb18227b5b6e588a
tracer-ics2022 <tracer@njuics.org>
2022-11-17 16:43:21 +0800

    >  compile NEMU 211300038 闫晨阳 Linux ycy-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:43:21 up 34 min,  1 user,  load average: 1.05, 0.82, 0.68

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -38,7 +38,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
       for(int j=0;j<ctl->h;j++)
         {
           //fb[((ctl->x+i)*300)+((ctl->y)+j)]=((uint32_t *)(uintptr_t)ctl->pixels)[ctl->x+ctl->y];
-          fb[((ctl->x+i)*300)+((ctl->y)+j)]=((uint32_t *)(uintptr_t)ctl->pixels)[i*32+j];
+          fb[((ctl->x+i)*300)+((ctl->y)+j)]=((uint32_t *)(uintptr_t)ctl->pixels)[i+j*32];
         }
     }
         //fb[((ctl->x)*300)+((ctl->y))]=((uint32_t *)(uintptr_t)ctl->pixels)[ctl->x+ctl->y];
